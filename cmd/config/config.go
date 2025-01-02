0ab329033b73e671b833f0d410a9542c31bcff4c
Gustavo Aguero Ferrari <gustyaguero21@gmail.com>
2025-01-02 17:11:26 -0300

    Commit: adding database

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -5,3 +5,9 @@ package config
 const (
 	Port = ":8080"
 )
+
+//sql queries
+
+const (
+	CreateTable = `CREATE TABLE USERS (id INTEGER PRIMARY KEY, name TEXT NOT NULL, surname TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL);`
+)
